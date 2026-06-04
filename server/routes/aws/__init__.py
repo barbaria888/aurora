be071bc7cd3428d099890187402c606465fb6c77
barbaria888 <hardik.arora_cs22@gla.ac.in>
2026-06-04 20:18:23 +0530

    feat(aws): scope DEV_SECURITYHUB_API_KEY env to aurora-server only and register cloudwatch_routes blueprint

--- server/routes/aws/__init__.py
+++ server/routes/aws/__init__.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 bp = Blueprint('aws', __name__)
 
-from . import aws_routes, auth, onboarding, securityhub_routes
+from . import aws_routes, auth, onboarding, securityhub_routes, cloudwatch_routes
 
 bp.register_blueprint(aws_routes.aws_bp)
 bp.register_blueprint(auth.auth_bp)
 bp.register_blueprint(onboarding.onboarding_bp)
 bp.register_blueprint(securityhub_routes.securityhub_bp, url_prefix='/securityhub')
-bp.register_blueprint(cloudwatch_bp)
+bp.register_blueprint(cloudwatch_routes.cloudwatch_bp)
